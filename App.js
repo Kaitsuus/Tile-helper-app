7a5bc2cbb7ee619487c4dab4b1656a46193d392b
Kai Jukarainen <kai.jukarainen@gmail.com>
2023-05-26 16:16:24 +0300

    add: styling changes

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React, { useState, createContext, useContext, useEffect } from 'react';
-import { NativeBaseProvider, Center } from "native-base";
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { NativeBaseProvider, Avatar } from 'native-base';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Image } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import Home from './screens/Home';
 import Grout from './screens/Grout';
 import Adhesive from './screens/Adhesive';
@@ -10,77 +10,46 @@ import WaterProof from './screens/WaterProof';
 import Plaster from './screens/Plaster';
 import Test from './screens/Test';
 
-
 const Stack = createStackNavigator();
 
 function HomeStack() {
+  const navigation = useNavigation();
+  const avatarUri = ''; // Replace with the URI or leave it empty
+
+  const navigateToHome = () => {
+    navigation.navigate('Home');
+  };
+
   return (
-    <Stack.Navigator defaultScreenOptions={Home}>
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{
-          headerTransparent: true,
-          headerTintColor: 'black',
-          headerTitle: () => (
-            <Image
-              source={require('./assets/apuriLogo.png')}
-              style={{ width: 50, height: 50 }}
-              resizeMode="contain"
-            />
-          )
-        }}
-      />
-      <Stack.Screen
-        name="Grout"
-        component={Grout}
-        options={{
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTintColor: '#ED7931',
-        }}
-      />
-      <Stack.Screen
-        name="Adhesive"
-        component={Adhesive}
-        options={{
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTintColor: '#ED7931',
-        }}
-      />
-      <Stack.Screen
-        name="WaterProof"
-        component={WaterProof}
-        options={{
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTintColor: '#ED7931',
-        }}
-      />
-      <Stack.Screen
-        name="Plaster"
-        component={Plaster}
-        options={{
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTintColor: '#ED7931',
-        }}
-      />
-      <Stack.Screen
-        name="Test"
-        component={Test}
-        options={{
-          headerStyle: {
-            backgroundColor: 'black',
-          },
-          headerTintColor: '#ED7931',
-        }}
-      />
+    <Stack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        headerTintColor: '#EF6F20',
+        headerLeft: () => (
+          <TouchableOpacity onPress={navigateToHome} style={{ paddingLeft: 10 }}>
+          <Image
+            source={require('./assets/apuriLogo.png')}
+            style={{ width: 50, height: 50, marginLeft: 10 }}
+            resizeMode="contain"
+          />
+          </TouchableOpacity>
+        ),
+        headerRight: () => (
+          <TouchableOpacity style={{ paddingRight: 15 }}>
+            <Avatar bg="#EF6F20" source={avatarUri ? { uri: avatarUri } : undefined}>
+              {avatarUri ? 'AJ' : 'A'}
+            </Avatar>
+          </TouchableOpacity>
+        ),
+      }}
+      initialRouteName="Home"
+    >
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Grout" component={Grout} />
+      <Stack.Screen name="Adhesive" component={Adhesive} />
+      <Stack.Screen name="WaterProof" component={WaterProof} />
+      <Stack.Screen name="Plaster" component={Plaster} />
+      <Stack.Screen name="Test" component={Test} />
     </Stack.Navigator>
   );
 }
