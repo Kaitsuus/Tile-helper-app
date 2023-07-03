ab4455ac5ac580edacfe6ac54f7f49c72d43b377
Kaitsuus <kai.jukarainen@gmail.com>
2023-07-03 10:33:31 +0300

    Add: JSDoc to NavigationButton

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -3,17 +3,33 @@ import { useNavigation } from '@react-navigation/native';
 import { Button } from 'native-base';
 import { RootStackParamList, HomeScreenNavigationProp } from '../types';
 
+/**
+ * @interface NavigationButtonProps
+ * @description Represents the props for the NavigationButton component.
+ * @property {keyof RootStackParamList} screenName - The name of the screen to navigate to.
+ * @property {string} title - The title of the button.
+ */
 type NavigationButtonProps = {
   screenName: keyof RootStackParamList;
   title: string;
 };
 
+/**
+ * @component NavigationButton
+ * @description The NavigationButton component.
+ * @param {NavigationButtonProps} props - The props for the NavigationButton component.
+ * @returns {React.FC} A React functional component.
+ */
 const NavigationButton: React.FC<NavigationButtonProps> = ({
   screenName,
   title
 }) => {
   const navigation = useNavigation<HomeScreenNavigationProp>();
 
+  /**
+   * @function navigateToScreen
+   * @description Navigate to the specified screen.
+   */
   const navigateToScreen = () => {
     navigation.navigate(screenName);
   };
