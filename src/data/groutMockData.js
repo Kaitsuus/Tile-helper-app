7f53377d8a4c21504ad5cdc1bd83e843d5eeb7b8
Kaitsuus <kai.jukarainen@gmail.com>
2023-07-11 09:07:55 +0300

    Add more products

--- src/data/groutMockData.js
+++ src/data/groutMockData.js
@@ -17,7 +17,7 @@ export const groutOptions = [
     label: 'Ardex G10 (1-10 mm)',
     value: 'Ardex G10',
     consumption: 1.46,
-    colors: ['valkoinen', 'pergamon', 'hopeanharmaa', 'vaaleanharmaa', 'hiekanharmaa', 'harmaa', 'sementinharmaa', 'kivenharmaa', 'basaltti', 'antrasiitti', 'jasmin', 'bahamabeige', 'hiakkabeige', 'jurabeige', 'harmaanruskea', 'balinruskea', 'tummanruskea'],
+    colors: ['White', 'pergamon', 'Silver grey', 'Light grey', 'Sand grey', 'Grey', 'Cement grey', 'Stone grey', 'Basalt', 'Anthracite', 'Jasmin', 'Bahamabeige', 'Sand beige', 'Jurabeige', 'Grey brown', 'Bali brown', 'Dark Brown'],
     url: 'https://ardex.fi/product/ardex-g-10/'
   },
   {
@@ -31,7 +31,28 @@ export const groutOptions = [
   label: 'Bostik Multi Grout (2-7 mm)',
   value: 'Bostik Multi Grout',
   consumption: 1.6,
-  color: ['Musta', 'Valkoinen', 'Marmorinvalkoinen', 'Vaaleanharmaa', 'Keskiharmaa', 'Harmaa'],
+  color: ['Black', 'White', 'White grey', 'Light grey', 'Medium grey', 'Grey'],
   url: 'https://www.bostik.com/finland/fi/catalog/product/construction/emea/finland/product-multi-grout/'
+  },
+  {
+  label: 'Schönox SF Design (1-10 mm)',
+  value: 'Schönox SF Design',
+  consumption: 1.45,
+  color: ['White', 'Silvergrey', 'Manhattan', 'Grey', 'Sandgrey', 'Platinum grey', 'Darkgrey', 'Graphite', 'Jasmine', 'Sandstone', 'Jurabeige', 'Caramel', 'Havanna'],
+  url: 'https://www.schonox.com/fi/tuotteet/?pc=234&p=5633'
+  },
+  {
+  label: 'Weber Classic Grout (1-8 mm)',
+  value: 'Weber Classic Grout',
+  consumption: 1.55,
+  color: ['11 White', '12 Marble', '13 Silver grey', '14 Smoke', '15 Concrete', '16 Grey,', '17 Medium grey', '18 Dark grey', '19 Anthracite', '20 Graphtie', '31 Cream', '32 Oak', '38 Leather'],
+  url: 'https://www.fi.weber/laatoitusratkaisut-ja-tuotteet/saumalaastit/weber-classic-grout-saumalaasti'
+  },
+  {
+  label: 'Casco Design Premium (1-10 mm)',
+  value: 'Casco Design Premium',
+  consumption: 1.45,
+  color: ['Grey', 'Light grey', 'Manhattan', 'Dark grey', 'White', 'Graphite', 'Platinum grey'],
+  url: 'https://www.casco.eu/global/casco/?pc=92&p=5610'
   }
 ];
